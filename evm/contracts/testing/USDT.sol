90a2283435f63a469a22c318b47cdbc87fc6975d
Victor Ionescu <113468466+gxmxni-hashflow@users.noreply.github.com>
2024-06-28 09:18:26 -0400

    Add USDC / USDT test token contracts. (#38)

---
+++ evm/contracts/testing/USDT.sol
@@ -0,0 +1,25 @@
+/**
+ * SPDX-License-Identifier: UNLICENSED
+ */
+pragma solidity 0.8.18;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol';
+
+contract USDT is ERC20Permit {
+    constructor(uint256 _totalSupply)
+    ERC20('Tether', 'USDT')
+    ERC20Permit('Tether')
+    {
+        _mint(msg.sender, _totalSupply);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+
+    function mint(uint256 value) external returns (bool) {
+        _mint(msg.sender, value);
+        return true;
+    }
+}
